088e2fcc9568ab56f0cf0a60b7e99a48a4d8ae9d
Leo <hamlap6@gmail.com>
2020-02-15 21:08:01 -0800

    finished loop practice

---
+++ unit_1/w01d3/homework/loops.js
@@ -0,0 +1,50 @@
+// 1.
+for (let i = 0; i <= 99; i++){
+    console.log(i);
+}
+
+// 2.
+for (let i = 99; i >= 0; i--){
+    console.log(i);
+}
+
+// 3.
+for (let i = 0; i <= 98; i+=2){
+    console.log(i);
+}
+
+// 4.
+for (let i = 98; i >= 0; i-=2){
+    console.log(i);
+}
+
+// 5. 
+for (let i = 0; i <= 99; i++){
+    if (i%2 !== 0){
+        console.log(i);
+    }
+}
+
+// 6.
+for (let i = 99; i >= 0; i--){
+    if(i%2 ==0){
+        console.log(i);
+    }
+}
+
+// 7. 
+for (let i = 49; i <= 72; i++){
+    console.log(i);
+}
+
+// 8.
+for (let i = 81; i >= 26; i--){
+    console.log(i);
+}
+
+// 9.
+for (let i = 3; i <= 90; i++){
+    if(i%3 === 0){
+        console.log(i);
+    }
+}
