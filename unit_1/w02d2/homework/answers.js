f495c57f6290c7d3de17736a84774d90f6c1b409
Leo <hamlap6@gmail.com>
2020-02-19 20:41:40 -0800

    fizzbuzz answered

--- unit_1/w02d2/homework/answers.js
+++ unit_1/w02d2/homework/answers.js
@@ -17,7 +17,17 @@ for (let i = 0; i <= 200; i++){
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
+for (let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+        console.log("fizz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else {
+        console.log(i);
+    }
+}
 
 ////////////////////////////////
 // Wild Wild Life
