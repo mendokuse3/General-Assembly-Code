05bb0ff95a613e52c9f0462c36976270164c5209
Leo <hamlap6@gmail.com>
2020-02-22 14:15:44 -0800

    completed verbal questions

---
+++ unit_1/w02d3/homework/solutions.js
@@ -0,0 +1,4 @@
+// VERBAL QUESTIONS
+// 1. An argument is the input when you are calling the function.  the parameter is the input represented within the function
+// 2. console.log simply prints to the console and is not useable.  return can make it so that the value of the function becomes whatever is returned and can be used within other functions.
+
