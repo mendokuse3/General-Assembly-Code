76664db6bf4ea201573d623dac790ea4a9fcdb3b
Leo <hamlap6@gmail.com>
2020-02-22 14:24:09 -0800

    palindrome completed

--- unit_1/w02d3/homework/solutions.js
+++ unit_1/w02d3/homework/solutions.js
@@ -2,3 +2,11 @@
 // 1. An argument is the input when you are calling the function.  the parameter is the input represented within the function
 // 2. console.log simply prints to the console and is not useable.  return can make it so that the value of the function becomes whatever is returned and can be used within other functions.
 
+// PALINDROME
+const checkPalindrome = string => {
+    if (string === string.split("").reverse().join("")){
+        return true;
+    }
+    return false;
+}
+console.log(checkPalindrome("radar"));
