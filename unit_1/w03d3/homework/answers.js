ccb27df01159cc6861a245df6eee501434f288d8
Leo <hamlap6@gmail.com>
2020-02-29 14:05:10 -0800

    crazy object complete

--- unit_1/w03d3/homework/answers.js
+++ unit_1/w03d3/homework/answers.js
@@ -24,3 +24,55 @@ const leo = {
     favDatatype: "string"
 }
 console.log(leo.hobby)
+
+// CRAZY OBJECT
+const crazyObject = {
+    taco: [{meat: 'steak',
+           cheese: ['panela', 'queso', 'chihuahua']},
+           {meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+          ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [{
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+      ]
+    }
+}
+console.log(
+    crazyObject.taco[1].salsa[5]
+)  
+
+console.log(
+    crazyObject.larry.quotes[0]
+);
+
+console.log(
+    crazyObject.larry.characters[2].favourtieHobby
+);
+
+console.log(
+    crazyObject.larry.nicknames[1]
+);
+
+console.log(
+    crazyObject.larry.characters[1]
+);
+
+crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk");
+console.log(
+    crazyObject.larry.quotes
+);
+
