45ec36a9537908735528be901e2d569867a208b8
Leo <hamlap6@gmail.com>
2020-02-29 13:56:53 -0800

    datatype refresher done

---
+++ unit_1/w03d3/homework/answers.js
@@ -0,0 +1,13 @@
+// ANSWER THE FOLLOWING
+// 1. datatype: boolean
+    // example: const lightIsOn = true;
+// 2. datatype: string
+    // ex: const userEmail = "mynameis@gmail.com"
+// 3. datatype: array and strings
+    // ex: const spaceship = ["hull", "laser blasters", .... ]
+// 4. datatype: array and strings
+    // ex: const class = ['name 1", 'name2', ...]
+// 5. datatype: object, array, and strings
+    // ex: const class = [{name: "name1", location: "location1"}, {name: "name2", location: "location2"}, ... ]
+// 6. datatype: array, object, string
+    // ex: const class = [{name: "name 1", location: "location 1", tvlist: ["show 1", "show 2"]}, {{name: "name 2", location: "location 2", tvlist: ["show 1", "show 2"]}]
