69810d1a777fe50888adee91ecdfbbe4ebdad04f
Leo <hamlap6@gmail.com>
2020-03-04 20:33:07 -0800

    combine objects, arrays, and functions

---
+++ unit_1/w04d2/homework/js_callbacks_and_array_methods/combining_datatypes.js
@@ -0,0 +1,42 @@
+const crayonBox = {
+    crayonArr: [
+        'white', 'peach', 'blue', 'skin'
+    ]
+}
+console.log(crayonBox.crayonArr[3])
+
+const bottle = {
+    cap: {
+        material: "metal",
+        color: "silver"
+    }
+}
+console.log(bottle.cap.material)
+
+const receipt = [
+    'bob',
+    2343,
+    obj = {
+        knife: {
+            price: 23,
+            name: "bob Kramer"
+        },
+        spoon: {
+            price: 2,
+            name: "ikea brand"
+        }
+    },
+    'floopin'
+]
+
+console.log(receipt[2].knife.name)
+
+const apartmentBuilding = [
+    'ken',
+    23432,
+    [
+        'hannah', 'alex', 'krystal', 'haidii'
+    ],
+    false
+]
+console.log(apartmentBuilding[2][3]);
