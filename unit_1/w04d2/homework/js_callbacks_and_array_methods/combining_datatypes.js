936a933f99e553478aa23804a0f24403413dba3f
Leo <hamlap6@gmail.com>
2020-03-05 12:01:44 -0800

    completed section 2

--- unit_1/w04d2/homework/js_callbacks_and_array_methods/combining_datatypes.js
+++ unit_1/w04d2/homework/js_callbacks_and_array_methods/combining_datatypes.js
@@ -117,3 +117,32 @@ console.log(calculate(1, 1, add));
 console.log(calculate(2,1, subtract));
 console.log(calculate(2,2, multiply));
 console.log(calculate(4, 2, divide));
+
+const bar = () => {
+    console.log('bar here');
+}
+const foo = () => {
+    console.log('foo here');
+}
+bar();
+foo();
+
+// foo();
+
+// const foo ()=>{
+//     console.log('hi');
+// }
+// // the error here says "Missing initializer in const declaration" which means that it is missing the = after foo.
+// // to fix it should be const foo = () => {code}
+// // after fixing that it will give error "cannot access 'foo' before initialization" which means that you cannot call a function before it is declared/initialized
+
+
+
+
+
+
+
+
+
+
+
