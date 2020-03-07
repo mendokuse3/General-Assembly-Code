34473e9a7615fdcc1244a4c013dbe80fb2c5d864
Leo <hamlap6@gmail.com>
2020-03-07 14:38:53 -0800

    created dougie

---
+++ unit_1/w04d3/homework/donut_adventure/adventure.js
@@ -0,0 +1,24 @@
+class Hero {
+    constructor(name){
+        this.name = name;
+        this.health = 100;
+        this.weapons = {
+            sprinkleSpray: 5,
+            sugarShock: 10
+        }
+        this.catchPhrases = ['i\'m fresher than day old pizza', 'you can\'t count my calories']
+    }
+    talkSass() {
+        console.log(this.catchPhrases[Math.floor(Math.random()*2)])
+    }
+    announceHealth() {
+    console.log(this.health);
+    }
+    fight() {
+        console.log("I\'m ready to rumble");
+    }
+}
+
+const dougie = new Hero("Dougie the donut");
+
+
