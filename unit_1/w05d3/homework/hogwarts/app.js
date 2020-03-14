d921b33e1fe0e56fcecb2adf75eb6b0c82f40250
Leo <hamlap6@gmail.com>
2020-03-14 14:20:31 -0700

    i added more elements.  My power grows

--- unit_1/w05d3/homework/hogwarts/app.js
+++ unit_1/w05d3/homework/hogwarts/app.js
@@ -1,11 +1,20 @@
 $(() => {
+    // year 1
     const $container = $('#container');
     console.log($container);
     const $h1 = $('<h1>').text("Hogwarts");
     // the h1 does not appear because it has not been appended
     $container.append($h1);
 
-
+    // year 2
+    $container.append($('<h2>').text("Leo"));
+    $container.append($('<h3>').text("Ravenclaw"));
+    const $pet = $('<h4>').text("Nikki");
+    $pet.addClass('cat');
+    $container.append($pet);
+    const $wand = $("<h4>").text("i dunno... a wandy wand");
+    $container.append($wand);
+    
 
 
 
