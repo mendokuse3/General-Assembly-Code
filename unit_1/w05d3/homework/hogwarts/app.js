1d6699f9e518bc4d47f7b5c55bc14cae61def946
Leo <hamlap6@gmail.com>
2020-03-14 14:47:31 -0700

    commit 3 - added list elements. that code didnt seem very dry'

--- unit_1/w05d3/homework/hogwarts/app.js
+++ unit_1/w05d3/homework/hogwarts/app.js
@@ -14,8 +14,18 @@ $(() => {
     $container.append($pet);
     const $wand = $("<h4>").text("i dunno... a wandy wand");
     $container.append($wand);
-    
 
+    // year 3
+    $container.append($('<ul>').attr('storage', 'trunk'));
+    $('ul').append($('<li>').text('butter beer'));
+    $('ul').append($('<li class=\"secret\">').text('invisibility cloak'));
+    $('ul').append($('<li class=\"secret\">').text('magic map'));
+    $('ul').append($('<li class=\"secret\">').text('time turner'));
+    $('ul').append($('<li class=\"cat\">').text('leash'));
+    $('ul').append($('<li>').text('Bertie Bott\'s Every Flavor [Jelly] Beans'));
+
+    
+    
 
 
 
