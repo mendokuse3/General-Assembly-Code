78d286a630da5158a64536ba8ff437a002b69f3d
Leo <hamlap6@gmail.com>
2020-03-18 19:19:02 -0700

    chapter 1 complete - made middle earth

--- unit_1/w06d2/homework/LOTR/scripts/fellowship.js
+++ unit_1/w06d2/homework/LOTR/scripts/fellowship.js
@@ -42,19 +42,21 @@ const makeMiddleEarth = () => {
   // console.log("Trying to make middle earth.");
 
   // 1. create a section tag with an id of middle-earth
-
+  const $midEarth = $('<section>').attr('id', 'middle-earth');
   // 2. append the section to the body of the DOM.
-
+  $('body').append($midEarth);
   // 3. use a for loop to iterate over the lands array that does the following:
+  for (let i = 0; i < lands.length; i++){
 
-  //   3a. creates an article tag (there should be one for each land when the loop is done)
-
-  //   3b. gives each land article an `id` tag of the corresponding land name
-
-  //   3c. includes an h1 with the name of the land inside each land article
-
-  //   3d. appends each land to the middle-earth section
-
+    //   3a. creates an article tag (there should be one for each land when the loop is done)
+    const $landArticle = $('<article>');
+    //   3b. gives each land article an `id` tag of the corresponding land name
+    $landArticle.attr('id', lands[i]);
+    //   3c. includes an h1 with the name of the land inside each land article
+    $landArticle.append($('<h1>').text(lands[i]));
+    //   3d. appends each land to the middle-earth section
+    $midEarth.append($landArticle);
+  }
 };
 
 // COMMIT YOUR WORK
