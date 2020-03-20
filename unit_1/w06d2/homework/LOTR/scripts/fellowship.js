217e9046a52b6c54a43dd8e8073da74eca8502d9
Leo <hamlap6@gmail.com>
2020-03-19 22:19:57 -0700

    chapter 9 complete - updated gandalf

--- unit_1/w06d2/homework/LOTR/scripts/fellowship.js
+++ unit_1/w06d2/homework/LOTR/scripts/fellowship.js
@@ -209,9 +209,9 @@ const forgeTheFellowShip = () => {
 const theBalrog = () => {
 
   // 1. change the 'Gandalf' text to 'Gandalf the White'
-
+  $('.buddy').eq(0).text('Gandalf the White');
   // 2. add a class "the-white" to this element
-
+  $('.buddy').eq(0).addClass('the-white');
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
 
 };
