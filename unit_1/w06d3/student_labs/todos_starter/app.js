aee7279a219335ce85d1fe6765ae3ad313c054b5
Leo <hamlap6@gmail.com>
2020-03-21 14:08:25 -0700

    user can now click complete button and add item to completed list

--- unit_1/w06d3/student_labs/todos_starter/app.js
+++ unit_1/w06d3/student_labs/todos_starter/app.js
@@ -5,8 +5,18 @@
 const addToDo = () => {
     const $toDoItem = $('<div>').addClass('to-do-item');
     $toDoItem.text($('#input-box').val());
+    $toDoItem.append($('<button>').attr('id', 'complete-btn').text("Completed"));
     $('#to-do-list').append($toDoItem);
+    completeToDO();
 }
+const completeToDO = () => {
+    $('#complete-btn').on('click', (event) => {
+        let $doneItem = $('.to-do-item').removeClass();
+        $doneItem.addClass('done-item');
+        $('.done-item').appendTo('#completed');
+    })
+}
+
 
 $(() => {
     $('form').on('submit', (event) => {
