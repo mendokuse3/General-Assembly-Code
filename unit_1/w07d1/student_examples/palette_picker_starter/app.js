30e34a24f9798a4a14988ec13195a11e7a89dcd2
Leo <hamlap6@gmail.com>
2020-03-25 15:27:41 -0700

    pre class commit

--- unit_1/w07d1/student_examples/palette_picker_starter/app.js
+++ unit_1/w07d1/student_examples/palette_picker_starter/app.js
@@ -1,1 +1,40 @@
 console.log('app.js loaded for palette picker');
+
+
+$(() => {
+    // cached dom elements
+    const $colorPalette = $('#color-palette');
+    const $myPalette = $('#my-palette');
+    const $generate = $('#generate');
+
+    // functions
+    const makePalette = () => {
+        $colorPalette.empty();
+        for (let i = 0; i < 150; i++){
+            const $div = $('<div>').addClass('square');
+            $div.css('background-color', randomColor());
+
+            $div.on('click', addColor);
+
+            $colorPalette.append($div);
+        }
+    }
+
+    const addColor = (event) => {
+        const color = $(event.currentTarget).css('background-color');
+        const $div = $('<div>').addClass('square').css('background-color', color);
+        $myPalette.append($div);
+    }
+    
+    const randomColor = () => {
+        const red = Math.floor(Math.random() * 255);
+        const green = Math.floor(Math.random() * 255);
+        const blue = Math.floor(Math.random() * 255);
+        return `rgb(${red}, ${green}, ${blue})`;
+    }
+
+    // make our palette
+    $generate.on('click', makePalette);
+
+
+})
--- unit_1/w07d1/student_labs/shape_clicker/js/app.js
+++ unit_1/w07d1/student_labs/shape_clicker/js/app.js
@@ -1,3 +1,44 @@
 console.log('shape clicker js file is connected');
 
+
+
+// let count = 0;
+
 //remember to set up your window onload!
+$(() => {
+    // thought i needed to make an array with all of these
+    const $circle = $('.circle');
+    const $square = $('.square');
+    const $invTriangle = $('.invTriangle');
+    const $octagon = $('.octagon');
+    const $heart = $('.heart')
+    
+    const $triangle = $('.triangle');
+    const array = [
+        "circle", "square", "triangle-down", "octagon", "heart", "triangle"
+    ]
+    const array2 = [
+        $circle, $square, $invTriangle, $octagon, $heart, $triangle
+    ]
+    let count = 0;
+
+    const changeShape = (event) => {
+        const obj = $(event.currentTarget);
+        if (count < array.length) {
+            obj.removeClass().addClass(array[count]);
+            count ++;
+        } else {
+            count = 0;
+            obj.removeClass().addClass(array[count]);
+            count ++;
+        }
+    }
+    $triangle.on('click', changeShape);
+    // $triangle.on('mouseenter', (event) => {$(event.currentTarget).css('border-bottom-color', 'black')})
+    // $triangle.on('mouseleave', (event) => {$(event.currentTarget).css('border-bottom-color', 'red')})
+    // $(`.${array[count]}`).on('mouseenter', (event) => {$(event.currentTarget).css('border-bottom-color', 'black')})
+    array2.map(function(item){
+        item.on('mouseenter', (event) => {$(event.currentTarget).css('border-bottom-color', 'black')});
+        item.on('mouseleave', (event) => {$(event.currentTarget).css('border-bottom-color', 'red')})
+    })
+})
