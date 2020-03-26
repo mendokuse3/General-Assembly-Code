473f949ea5b59ea5aeb5357c629dfd30de3d8d9e
Leo <hamlap6@gmail.com>
2020-03-25 18:35:07 -0700

    made each square clickable

--- unit_1/w07d2/homework/tic_tac_toe/app.js
+++ unit_1/w07d2/homework/tic_tac_toe/app.js
@@ -1,19 +1,18 @@
 $(() => {
-    // const $squares = $('.square');
     const $container = $('#container')
 
     const generateBoard = () => {
         for (let i = 0; i < 9; i++){
             const $square = $('<div>').addClass('square');
+            $square.on('click', changeSquare);
             $container.append($square);
         }
     }
     
     const changeSquare = event => {
-        // $(event.currentTarget).css('background', 'grey');
+        $(event.currentTarget).css('background', 'grey');
 
     }
 
-    // $squares.on('click', changeSquare)
     generateBoard();
 })
