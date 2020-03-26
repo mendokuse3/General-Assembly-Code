fcda14a202488250cbb260c9c262d127d280a56a
Leo <hamlap6@gmail.com>
2020-03-25 18:14:14 -0700

    remade grid

--- unit_1/w07d2/homework/tic_tac_toe/app.js
+++ unit_1/w07d2/homework/tic_tac_toe/app.js
@@ -1,8 +1,19 @@
 $(() => {
-    const $squares = $('.square');
-    
-    $squares.on('click', (event) => {
-        $(event.currentTarget).css('background', 'grey')
-    })
+    // const $squares = $('.square');
+    const $container = $('#container')
 
+    const generateBoard = () => {
+        for (let i = 0; i < 9; i++){
+            const $square = $('<div>').addClass('square');
+            $container.append($square);
+        }
+    }
+    
+    const changeSquare = event => {
+        // $(event.currentTarget).css('background', 'grey');
+
+    }
+
+    // $squares.on('click', changeSquare)
+    generateBoard();
 })
