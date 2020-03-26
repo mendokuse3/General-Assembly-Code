d3429a0d7d64178236710fd2be894649efe03214
Leo <hamlap6@gmail.com>
2020-03-25 18:43:02 -0700

    made suarees x or o

--- unit_1/w07d2/homework/tic_tac_toe/app.js
+++ unit_1/w07d2/homework/tic_tac_toe/app.js
@@ -1,6 +1,7 @@
 $(() => {
     const $container = $('#container')
     // const $squares = $('.square');
+    let counter = 1;
 
     const generateBoard = () => {
         for (let i = 0; i < 9; i++){
@@ -15,7 +16,22 @@ $(() => {
         // give each square a div
         // give each div text "x"
         const ths = $(event.currentTarget);
-        const marker = $('<div>').addClass('marker').text('x');
+        const marker = $('<div>').addClass('marker');
+        // x or o logic
+            // set a counter = 0; 
+            // if statement
+                // if counter % 2 === 0 then marker.text(x)
+                // counter ++
+            // else if
+                // if counter % 2 !== 0 then marker.text(o)
+                // counter ++
+        if (counter % 2 === 0){
+            marker.text('x');
+            counter ++;
+        } else if (counter % 2 !== 0){
+            marker.text('o');
+            counter ++;
+        }
         ths.append(marker);
 
 
