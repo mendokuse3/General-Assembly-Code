6f28d576b5908b66b51f727e9954428b6e2f1b24
Leo <hamlap6@gmail.com>
2020-03-26 20:16:01 -0700

    working on logic, finally able to retrieve marker value

--- unit_1/w07d2/homework/tic_tac_toe/app.js
+++ unit_1/w07d2/homework/tic_tac_toe/app.js
@@ -1,7 +1,9 @@
 $(() => {
     const $container = $('#container')
-    // const $squares = $('.square');
+    const $squares = $('.square');
+    const $mark = $('.marker');
     let counter = true;
+    let array = [];
 
     const generateBoard = () => {
         for (let i = 0; i < 9; i++){
@@ -27,17 +29,47 @@ $(() => {
                 // counter ++
         if (counter){
             marker.text('x');
+            array.push(marker);
+            // $.merge(array, marker)
+            checkWin();
             counter = !counter;
         } else if (!counter){
             marker.text('o');
+            array.push(marker);
+            // $.merge(array, marker)
+            checkWin();
             counter = !counter;
         }
         ths.append(marker);
         // this makes the clicked square unclickable by removing the event handler
-        ths.off()
+        ths.off();
+        // console.log($('.marker').eq(0).text());
+
+    }
+
+
+// for win condition logic, in order to display x or o has won teh game
+// use the counter: if counter is true, x has won/ if counter is false o has won
+
+    // for column win
+        // x + 3
+
+    // for diagonal win
+        // x + 4
+    // for row win
+        // 1-3, 4-6,
+    const checkWin = () => {
+        // if($mark.eq(0).text() === $mark.eq(1).text() === $mark.eq(2).text()){
+        //     console.log('gameover');
+        // }
+        // console.log($mark)
+        // console.log(array[0].text());
+        array.forEach(x => console.log(x.text()));
+    }
+    
 
     
-    }
+    
 
     generateBoard();
 })
